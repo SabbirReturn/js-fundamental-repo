bd95d8e6c3e90c34afd9a9b7ff12a19b32f700ba
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-03-19 04:33:34 +0600

    loop_task

---
+++ array/loop_task.js
@@ -0,0 +1,47 @@
+// let num = 0;
+// while(num<60){
+//     console.log('I will invest at least 6 hrs every single day for next 60 days!')
+//     num+=1;
+//     console.log(num);
+// }
+// let num = 61;
+// while( num<=100){
+//     console.log(num);
+//     num+=1;
+//     if(num % 2===1){
+//         console.log('odd number:',num);
+//     }
+// }
+// let num = 78;
+// while( num<=98){
+//     console.log(num);
+//     num+=1;
+//     if(num % 2===0){
+//         console.log('odd number:',num);
+//     }
+// }
+
+
+let num = 81;
+const sum =0;
+while( num<=131){
+    console.log(num);
+    num+=1;
+    if(num % 2 !==0){
+        console.log('odd number:',num);
+        // sum = sum + num;
+        // console.log(sum);
+    }
+}
+// let sumOfOddNumbers = 0;
+
+// // Loop through the numbers from 81 to 131
+// for (let num = 81; num <= 131; num++) {
+//     // Check if the number is odd
+//     console.log(num);
+//     if (num % 2 !== 0) {
+//         // If it's odd, add it to the sum
+//         sumOfOddNumbers += num;
+//         console.log(sumOfOddNumbers)
+//     }
+// }
