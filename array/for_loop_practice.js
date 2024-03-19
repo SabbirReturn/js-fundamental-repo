b72fa1a24c92a2e5cb08572dc94fa3adf02102fa
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-03-20 03:00:55 +0600

    for_loop_practice

---
+++ array/for_loop_practice.js
@@ -0,0 +1,33 @@
+// for( let i = 0; i<=60; i+=1 ){
+//     console.log('I will invest at least 6 hrs every single day for next 60 days!');
+// }
+// ..........Odd number_ for loop............
+// for( let i = 61; i<=100; i+=1){
+//     console.log(i);
+//     if( i % 2 !=0){
+//         console.log('odd num:',i);
+//     }
+    
+// }
+// ............Even num_for loop.............
+// let sum = 0;
+// for( let i = 78; i<=98; i+=1){
+//     console.log(i);
+//     if( i % 2===0){
+//         console.log('even num:',i);
+//         sum = sum + i;
+//     }
+//     console.log(sum);
+    
+// }
+
+// ...............sum of odd number...........
+let sum = 0;
+for( let i= 81; i<=131 ; i+=1){
+    console.log(i)
+    if(i % 2 !=0){
+        console.log('odd num:',i);
+        sum = sum+i;
+    }
+    console.log('sum num:',sum);
+}
