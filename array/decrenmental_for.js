8a51027f1180d8610655c15bfb2488cb22de655a
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-03-20 02:40:50 +0600

    decremental_for

---
+++ array/decrenmental_for.js
@@ -0,0 +1,10 @@
+// .........decremental for............
+
+for ( let i= 10; i>=0; i-=1){
+    // console.log(i);
+}
+// .........incremental for............
+
+for ( let i= 0; i<=10; i+=1){
+    console.log(i);
+}
