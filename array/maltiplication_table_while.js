f2f8e08bfc9ee425a587d1247e0dacfcbcb885f5
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-03-20 16:48:54 +0600

    maltiplication_for_loop

---
+++ array/maltiplication_table_while.js
@@ -0,0 +1,22 @@
+// let i =0;
+// const num =5;
+// while(i<=10){
+//     // console.log(i);
+//     i+=1;
+//     const result = num * i;
+//     console.log(`${num} * ${i} = ${result}`);
+// }
+// const num= 5;
+// for ( let i=0; i<=10; i+=1){
+//     console.log(i);
+//     const result = num * i;
+//     // console.log(`${num} * ${i} = ${result}`);
+//     console.log(`${num} * ${i} = ${result} `);
+// }
+
+const num = 6;
+for(let i=0 ; i<=10 ; i+=1){
+    // console.log(i);
+    const maltiplication = num * i;
+    console.log(`${num} * ${i} = ${maltiplication}`);
+}
