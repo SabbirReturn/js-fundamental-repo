1c3725a8f359f7b42c22a9b62b0cb7e01de73e41
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-03-20 22:21:21 +0600

    different_js

---
+++ array/different.js
@@ -0,0 +1,38 @@
+// let sum = 0;
+// for ( let i = 0; i<=30; i+=1){
+//     if(i % 3 ===0){
+//         console.log(i);
+//         sum = sum + i;
+        
+//     }
+// }
+// console.log('sum num:', sum);
+
+
+// .......using break................
+// for(let i =0; i<=10; i+=1){
+//     console.log(i);
+//     if(i>=5){
+//         break;
+//     }
+// }
+
+// let i= 0;
+// while(i<=10){
+//     // console.log(i);
+//     i+=1;
+//     if(i>=5){
+//         break;
+//     }
+// }
+
+let i= 54;
+while(i>=25){
+    console.log(i)
+   
+    i-=1
+    if(i<50){
+        break;
+    }
+    
+}
