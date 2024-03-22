8fadb4f90fd6aa413c4e78d54398533c04a80d01
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-03-22 22:46:01 +0600

    case.js

---
+++ string-object/case.js
@@ -0,0 +1,26 @@
+const school = 'Bheramara Pilot High School'
+// lowercase
+// console.log(school.toLowerCase());
+// upercase
+// console.log(school.toUpperCase());
+
+
+// upercase/lowercase fact
+// const subject = 'Math'
+// const book = 'math';
+// if ( subject.toUpperCase()===book.toUpperCase()){
+//     console.log('pass korbo')
+// }
+// else{
+//     console.log('fail korbo')
+// }
+
+
+const drink= ' water';
+const liqued = 'water';
+if(drink.trim() === liqued.trim()){
+    console.log('pani khabo');
+}
+else{
+    console.log('muri khaw');
+}
