5da5e37dc40a5c2bd9588609d2c7ecf455fb1183
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-03-23 02:19:02 +0600

    slice_join_split

--- string-object/slice.js
+++ string-object/slice.js
@@ -1,2 +1,14 @@
-const country ='Bangladesh';
-console.log(country.slice(0,6));
+// slice
+// const country ='Bangladesh';
+// console.log(country.slice(0,6));
+
+// splite = alada alada vag kora 
+// const schoolName = 'Bheramara Pilot High School';
+// console.log(schoolName.split(' '));
+// const friends ='Sabbir hasan robin';
+// console.log(friends.split(','));
+
+
+// join== add kora
+// const friends =['Sabbir','Hasan','Robin'];
+// console.log(friends.join('|'));
