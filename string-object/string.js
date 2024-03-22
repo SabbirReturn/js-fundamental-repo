90317af41c623adfe641c0ee2fab21ffbe1ef1fe
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-03-22 22:13:03 +0600

    Array

---
+++ string-object/string.js
@@ -0,0 +1,12 @@
+const country = 'Bangladesh';
+const district = "Kushtia";
+// ``---powerfull jinis
+const thana = `Bheramra`;  
+
+const numbers = [12,13,14,15,16]
+console.log(numbers);
+numbers[1] = 25;
+console.log(numbers);
+for(const number of numbers){
+    console.log(number);
+}
