2f5101c78b08dcc5142a825ae8243f9ae6677929
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-03-22 22:15:58 +0600

    String

--- string-object/string.js
+++ string-object/string.js
@@ -1,12 +1,18 @@
-const country = 'Bangladesh';
+// const country = 'Bangladesh';
 const district = "Kushtia";
 // ``---powerfull jinis
 const thana = `Bheramra`;  
+// array is the changable element
+// const numbers = [12,13,14,15,16]
+// console.log(numbers);
+// numbers[1] = 25;
+// console.log(numbers);
+// for(const number of numbers){
+//     console.log(number);
+// }
 
-const numbers = [12,13,14,15,16]
-console.log(numbers);
-numbers[1] = 25;
-console.log(numbers);
-for(const number of numbers){
-    console.log(number);
-}
+// string is not changeble element
+const country = 'Bangladesh';
+console.log(country);
+country[1]= h;    
+// h nor defiend
