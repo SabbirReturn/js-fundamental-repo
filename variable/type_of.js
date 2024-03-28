2850b6d68a6459922fa03ac7c229dc8f98a45a7a
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-03-28 21:32:54 +0600

    type_of

---
+++ variable/type_of.js
@@ -0,0 +1,14 @@
+// string type
+const name = 'Sabbir Hasan'
+// console.log(name);
+// console.log(typeof name);
+
+// number type
+var age = 24;
+// console.log(age);
+// console.log(typeof age);
+
+// boolean type
+const isPassed = true;
+console.log(isPassed);
+console.log(typeof isPassed);
