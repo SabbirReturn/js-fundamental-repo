6239f2fa6c500ee4aa8bce7d612f05519e06d932
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-11 02:56:21 +0600

    nasted_if

---
+++ condition/nasted_if.js
@@ -0,0 +1,22 @@
+const money = 0;
+if(money>300){
+    console.log('Go to cineplex');
+}
+else{
+    if(money>200){
+        console.log('Chol chondrima jai');
+    }
+    else{
+        if(money>100){
+            console.log('fuska khawya chara upai nai');
+        }
+        else{
+            if(money>0){
+                console.log('kola kha');
+            }
+            else{
+                console.log('Tui amar bondhu na');
+            }
+        }
+    }
+}
