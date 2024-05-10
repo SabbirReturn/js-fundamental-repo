36603222caa9bf884928749f780613a7105263be
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-10 19:16:18 +0600

    if.js

---
+++ condition/if.js
@@ -0,0 +1,16 @@
+// ...........structer...........
+
+// if(condition){
+//            execute code if condition above is true
+//       }
+//   else{
+//             execute code if condition above is false
+//       }
+
+var weight = 20;
+if(weight<=20){
+    console.log('I will carry it by myself');
+}
+else{
+    console.log('I will rent a rickshaw');
+}
