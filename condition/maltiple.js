63b718f792184bf9616f65b0dfd48c0212770848
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-10 20:36:00 +0600

    maltiple.js

---
+++ condition/maltiple.js
@@ -0,0 +1,18 @@
+var salary = 25000;
+var isBCS = true;
+var hasCar = false;
+
+// if(salary>20000) {
+//     console.log('accept');
+// }
+// else{
+//     console.log('reject');
+// }
+
+// ..................complex..............
+if((salary>20000 & hasCar==true)|| isBCS == true){
+    console.log('accept');
+}
+else{
+    console.log('reject');
+}
