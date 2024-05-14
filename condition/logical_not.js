fade3dd7c61a0c39ed4fd9b183d4e8adb92cb423
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-15 02:58:35 +0600

    logical_not

---
+++ condition/logical_not.js
@@ -0,0 +1,17 @@
+// let price = 1000;
+// const isLeader = true;
+// if(isLeader){
+//     console.log(price = 0)
+// }
+// else{
+//     console.log(price = price + 1000);
+// }
+
+let price = 1000;
+const isLeader = true;
+if(!isLeader){
+    console.log(price = 0)
+}
+else{
+    console.log(price = price + 1000);
+}
