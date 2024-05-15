4771e3b4d80efb9af808eca14be4edccec8f1bd6
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-15 07:02:31 +0600

    Grading_calculator

---
+++ condition/grading_calculator.js
@@ -0,0 +1,16 @@
+const mark = 69;
+if(mark>=90){
+    console.log("A");
+}
+else if(mark>=80){
+    console.log("B");
+}
+else if(mark>=70){
+    console.log("B");
+}
+else if(mark>=60){
+    console.log("B");
+}
+else{
+    console.log("F")
+}
