0b8ada645c077183c0b46e2abe9b5f3702045ad3
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-16 00:54:37 +0600

    tarney

--- condition/simple_ternary.js
+++ condition/simple_ternary.js
@@ -1,10 +1,15 @@
-var num1 = 2;
+var num1 = 22;
 var num2 = 12;
-if(num1>num2){
-    var num1 = num1 *2;
-    console.log(num1);
-}
-else{
-    var sum = num1 + num2;
-    console.log ("sum:",sum);
-}
+// if(num1>num2){
+//     var num1 = num1 *2;
+//     console.log(num1);
+// }
+// else{
+//     var sum = num1 + num2;
+//     console.log ("sum:",sum);
+// }
+
+
+// ...............simple tarnery..................
+result=num1>num2 ? num1 *2 : num1 + num2;
+console.log(result);
