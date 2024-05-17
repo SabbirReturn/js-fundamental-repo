b56f9d133e9ba9fb8b4f70bf3cefa6d85d1ce5af
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-17 20:46:19 +0600

    index.js

---
+++ Array/length.js
@@ -0,0 +1,15 @@
+let numbers = [22,33,44,55,66,778,88,999,1];
+console.log(numbers);
+// console.log(numbers.length);
+
+
+// ...................get element value by index................
+
+// console.log(numbers[1]);
+// let fourthNumber = numbers[3];
+// console.log(fourthNumber);
+
+
+// ............set element value by index...............
+numbers[1]= 888;
+console.log(numbers);
