85c7f73d19d4cb27567a82d96a40b51d694de6db
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-17 21:17:32 +0600

    shift_unshift

---
+++ Array/shift_unshift.js
@@ -0,0 +1,7 @@
+const friends = ['Sabbir','Hasan','Robin','Mahfuza','Misu'];
+console.log(friends);
+const add1 = friends.unshift('Ghuma');
+console.log(friends);
+const out1 = friends.shift();
+console.log(friends);
+console.log(out1);
