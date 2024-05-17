99c202db2f406eb78d9b1641549534ce802c0f9d
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-18 03:44:48 +0600

    Includes

--- Array/includes.js
+++ Array/includes.js
@@ -2,3 +2,10 @@ const friends = ['Sabbir','Hasan','Robin','Mahfuza','Misu'];
 console.log(friends.includes('Hasan'))
 const recheck = friends.includes('akter');
 console.log(recheck);
+
+if(friends.includes('Sabbir')){
+    console.log('Go to mawya');
+}
+else{
+    console.log('Ghumai thako');
+}
