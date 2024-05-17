d7d0359e65a66dccd63f8e0871f4f148ffdbbfa2
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-18 04:11:53 +0600

    array_practice

---
+++ Array/array_practice.js
@@ -0,0 +1,51 @@
+// ................Declare an array.............
+
+// let fruitsName = ['Mango','Banana','Lichu','Khathal','Tormuj']
+// console.log(fruitsName);
+// console.log(fruitsName[2]);
+// fruitsName[2]= 'Jambura'
+// console.log(fruitsName);
+
+
+// ...................Add or remove elements...................
+
+// let tourismSpote = ['Coxs Bazar','Bandarbon','Ranggamati'];
+// console.log(tourismSpote);
+// let addtourismSpot = tourismSpote.push('Khagrasory');
+// addtourismSpot = tourismSpote.push('mujibnagar');
+// addtourismSpot = tourismSpote.push('Sundarban');
+// let out1 = tourismSpote.pop()
+// console.log(out1);
+// console.log(tourismSpote);
+
+
+// ...................................Checking Array Membership with ‘includes’..................
+
+// let booksName = ['Physics','Chemistry','Biology','Math'];
+// console.log(booksName.includes('Chemistry'));
+// console.log(booksName.includes('javascript'));\
+
+
+// ..............................Checking if it's an Array...................
+
+// let age = 24;
+// let studentName= ['Ebon','Hamdi','Samir','Sadab']
+// let studentNumber = 4;
+// console.log(Array.isArray(age));
+// console.log(Array.isArray(studentName));
+// console.log(Array.isArray(studentNumber));
+
+// ..................................Combining Arrays........................
+
+let name =['Sabbir','Hasan'];
+let name1 = ['Robin','Hasan'];
+let fullname = name1.concat(name1);
+console.log(name);
+console.log(name1)
+console.log(fullname);
+
+// let name1 = ['Sabbir'];
+// let name2 = ['Hasan'];
+// let name3 = ['Robin'];
+// let fullname = name1.concat(name2,name3);
+// console.log(fullname);
