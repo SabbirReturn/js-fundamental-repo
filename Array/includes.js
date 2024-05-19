0845dfd67ce0bacf592aa819cc03dac367147209
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-19 23:04:55 +0600

    includes

--- Array/includes.js
+++ Array/includes.js
@@ -1,11 +1,17 @@
 const friends = ['Sabbir','Hasan','Robin','Mahfuza','Misu'];
-console.log(friends.includes('Hasan'))
-const recheck = friends.includes('akter');
-console.log(recheck);
+// console.log(friends.includes('Hasan'))
+// const recheck = friends.includes('akter');
+// console.log(recheck);
 
+// if(friends.includes('Sabbir')){
+//     console.log('Go to mawya');
+// }
+// else{
+//     console.log('Ghumai thako');
+// }
 if(friends.includes('Sabbir')){
-    console.log('Go to mawya');
+    console.log('Tahole ami beyete raji')
 }
 else{
-    console.log('Ghumai thako');
+    console.log('bia korbo na')
 }
