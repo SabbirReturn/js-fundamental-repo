306c137bd292f97541e10d80632213336d77f677
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-21 23:33:41 +0600

    mixed.js

--- condition/mixed.js
+++ condition/mixed.js
@@ -1,24 +1,22 @@
-let names = ['sabbir','hasan','robin'];
+let friends = ['sabbir','hasan','robin'];
 let price = 1000;
-let isStudent = true;
-if(names.inclueds('sabbir')){
+let isStudent = false;
+if(friends.includes('sabbir')){
     // 50% discount
     let discountAmount = price * 50/100;
     let payAmount = price - discountAmount;
     console.log(payAmount);
-        if(isStudent===true){
-             // 70% discount
-            let discountAmount = price * 70/100;
-            let payAmount = price - discountAmount;
-            console.log(payAmount);
-     }
-     else{
-        let payAmount = price;
+    if(isStudent === true){
+        // 70% discount
+        let discountAmount = price * 70/100;
+        let payAmount = price - discountAmount;
         console.log(payAmount);
-     }
+    }
+    else{
+        let payAmount = price;
+    }
 }
 else{
     let payAmount = price;
     console.log(payAmount);
 }
-
