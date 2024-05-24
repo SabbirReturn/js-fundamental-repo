9d3bb69a69966dce0c55e20d48e377e5dfe81038
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-24 17:22:50 +0600

    how to using loop

---
+++ Loop/while_loop.js
@@ -0,0 +1,15 @@
+// let number = 8;
+// if(number<10){
+//     console.log('Set the big');
+// }
+// else{
+//     console.log('Muri khaw');
+// }
+
+
+// ...................While loop................
+let num = 0;
+while(num<5){
+    console.log('looping',num);
+    num +=1;
+}
