172104bebff801078841d92d31569b01e32ef496
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-24 19:44:53 +0600

    sum

--- Loop/sum.js
+++ Loop/sum.js
@@ -1,4 +1,9 @@
-let num = 50;
-while(num<100){
-    console.log
+let num = 1;
+let sum =0;
+
+while(num<10){
+    console.log(num);
+     sum = sum + num;
+    console.log('total:',sum);
+    num++;
 }
