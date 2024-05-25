b6c047ab18ce896aa0dc07af90a21b5c6bb61a4f
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-26 04:03:10 +0600

    for_loop

---
+++ Loop/for_loop.js
@@ -0,0 +1,15 @@
+// ........for loop structure.............
+
+// for( 1st part ; 2nnd part ; 3rd part){
+
+// }
+// 1st part = variable diclaration
+// 2nd part = condition
+// 3rd part = incriment
+
+for(let i = 0; i<20 ; i++){
+    console.log(i);
+    if(i % 2 ===0){
+        console.log('even num:',i);
+    }
+}
