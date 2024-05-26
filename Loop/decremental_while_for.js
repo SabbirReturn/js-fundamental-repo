e0b2f58ff0034e36c0bcdd109f0aac38b7476efb
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-27 02:41:35 +0600

    decremental

---
+++ Loop/decremental_while_for.js
@@ -0,0 +1,14 @@
+// ................while loop...............
+// let i = 20;
+// while(i>=0){
+//     console.log(i);
+//     i--
+// }
+
+// ...........for loop...................
+let sum = 0;
+// for(let i = 20; i>=0; i--){
+//     console.log(i);
+//     sum+=i;
+//     console.log('sum:',sum);
+// }
