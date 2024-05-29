850f91c21857801f7d4c7fb2c82c2b794eea1cc9
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-30 03:14:59 +0600

    break_practice

---
+++ Loop/break_practice.js
@@ -0,0 +1,21 @@
+// ................Write a loop 1 to 200. Use break to exit the loop once you find 100................
+// for( let i = 1 ; i<=200; i++){
+//     if(i>100){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// ............Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100............
+// let sum =0;
+// for( let i= 1; i< 100; i++){
+//     // console.log(i);
+//     sum = sum +i;
+    
+//     if(sum >100){
+//         break;
+//     }
+//     console.log('sum:',sum);
+//     // console.log(i);
+// }
+
