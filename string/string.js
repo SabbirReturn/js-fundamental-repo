e9c6cf98877c4a214c85b6f1f31c70543a1d0dbf
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-31 02:03:26 +0600

    String

---
+++ string/string.js
@@ -0,0 +1,19 @@
+// ..............array is mutable
+// const numbers = [1,11,21,22,23,24];
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[2]);
+// numbers[2]=2;
+// console.log(numbers);
+
+// string is immuted 
+const country = 'Dhaka';
+const district = 'Dhaka';
+const thana = new String('kushtia');
+console.log(typeof country);
+console.log(typeof district);
+console.log(thana);
+console.log(country.length);
+console.log(country[0]);
+country[0]= "f";
+console.log(country);
