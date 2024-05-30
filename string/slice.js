94338c240e34efe8e6ad510a527f26dfad6b2d3e
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-31 03:53:21 +0600

    slice_concat_join_split

---
+++ string/slice.js
@@ -0,0 +1,14 @@
+// let country = 'Bangladesh';
+// console.log(country.slice(0,6));
+
+// let hisName = 'My name is Sabbir Hasan';
+// console.log(hisName.split('s'));
+
+let firstName = 'Sabbir';
+let lastName = 'Hasan';
+// let fullName = 'Sabbir' + ' ' +'Hasan';
+let fullName = firstName.concat(' '). concat(lastName);
+// console.log(fullName);
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.join(' and '))
