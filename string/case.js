64aba1b0cbfe74835fa6bb7b3e9998f6a98178b8
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-05-31 03:27:36 +0600

    case

---
+++ string/case.js
@@ -0,0 +1,23 @@
+// let schoolName = 'Bheramara Pilot High School'
+// console.log(schoolName);
+// console.log(schoolName.toLocaleLowerCase());
+// console.log(schoolName.toLocaleUpperCase());
+
+
+// let subject = 'Chemistry';
+// let book = 'chemistry';
+// if(subject.toLocaleLowerCase ===book.toLocaleLowerCase){
+//     console.log('Poira fatai feelmu')
+// }
+// else{
+//     console.log('Fail');
+// }
+
+let drink = ' water';
+let liquid = 'water';
+if(drink.trim === liquid.trim){
+    console.log('Panir opor name jibon')
+}
+else{
+    console.log('sumodrer pani nonta');
+}
