ead7ea1c4faf1d540acd7f446e7c787c9ba2466b
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-06-02 02:27:50 +0600

    property_access

---
+++ Object/property.js
@@ -0,0 +1,17 @@
+const person = {
+    name : 'Sabbir Hasan',
+    age : 25,
+    isstudent : true,
+    profession : 'Developer',
+    address : ' Uttara',
+}
+// console.log(person);
+// propert access with dot system............
+
+// const propName = person.profession;
+// console.log(person.profession)
+// console.log(propName);
+
+// poperty access with braket system................
+const propName = person['profession'];
+console.log(propName);
