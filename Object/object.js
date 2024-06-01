fdf0513a8c8e96e9856122743cecad33d5d99494
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-06-02 02:06:23 +0600

    How to write an object

---
+++ Object/object.js
@@ -0,0 +1,16 @@
+// ............How to diclear a object...............
+// const mobile = {
+//     brand : 'Samsung',
+//     model : 'J8',
+//     colour : 'Purple',
+//     owner : 'Sabbir',
+// }
+
+
+const monitor = {
+    brand : 'Hp',
+    price : 10000,
+    colour : 'Black',
+    iswell : true,
+}
+console.log(monitor);
