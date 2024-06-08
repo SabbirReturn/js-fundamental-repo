d17bf4a23fdc59e444b99c4ddba90a500f115bda
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-06-08 15:57:09 +0600

    loop

---
+++ Array/loop.js
@@ -0,0 +1,20 @@
+const friends = ['Sabbir','Hasan','Zaglul','Emon']
+// for( let i = 0; i<friends.length; i++){
+//     console.log(i,friends[i]);
+// }
+// let i= 0;
+// while(i<friends.length){
+//     console.log(i,friends[i]);
+//     i++
+// }
+
+const number = [11,12,13,14,15,16,17,18,19];
+// for( let i= 0; i<number.length; i++){
+//     console.log(i, number[i]);
+// }
+
+let n = 0;
+while(n<number.length){
+    console.log(n, number[n]);
+    n++
+}
